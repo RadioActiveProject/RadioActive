255796c13023cdfe30a37d1fa3293e22893b1d0c
RadioActiveProject <164354568+RadioActiveProject@users.noreply.github.com>
2024-05-15 21:45:09 +0300

    Update station.py

--- station.py
+++ station.py
@@ -19,6 +19,7 @@ class Station:
 
     def send_and_recv(self, id, pw, command):
         msg = f"{id}-{pw}-{COOKIE}-{command}"
+        # This encrpytion is super strong. Don't even try to break it ...
         enc_message = encrypt(msg.encode('utf-8'), KEY, IV)
         self.client_socket.send(enc_message)
         response = self.client_socket.recv(1024).decode('utf-8')
